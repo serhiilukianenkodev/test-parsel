bbb0f2a4227dbad86cf4e30e7a41264d44bb2606
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2022-08-06 21:47:07 +0000

    Deploying to gh-pages from @ serhiilukianenkodev/test-parsel@93d72a15d9bb7c98a0505666a84bc263bfba960b 🚀

---
+++ index.5ccc3897.js
@@ -0,0 +1,2 @@
+fetch("http://localhost:3000/profile").then((o=>o.json())).then(console.log);
+//# sourceMappingURL=index.5ccc3897.js.map
---
+++ index.dd666295.js
@@ -0,0 +1,2 @@
+fetch("http://localhost:3000/profile").then((function(o){return o.json()})).then(console.log);
+//# sourceMappingURL=index.dd666295.js.map
