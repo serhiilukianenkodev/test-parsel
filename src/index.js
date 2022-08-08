ad4381ad3188ef987ed2a08a8ba6d83a44204039
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-08-08 19:52:07 +0300

    add db.json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
-fetch('http://localhost:3000/profile').then(r => r.json()).then(console.log)
+const BASE_URL = 'http://localhost:3000';
+
+// function fetchPosts (){
+//  return   fetch(`${BASE_URL}/posts/`).then(r => r.json())
+// }
+
+// function fetchPostById(postId){
+//     return   fetch(`${BASE_URL}/posts/${postId}`).then(r => r.json())
+
+// }
+
+// fetchPostById(1)
+
+const newPost = {
+    "title": "test 22",
+    "author": "Serhii dev",
+}
+
+function addPost (post){
+const options = {
+    method: 'POST',
+    body: JSON.stringify(post),
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+
+return fetch(`${BASE_URL}/posts/`, options).then(r => r.json())
+
+}
+
+addPost(newPost).then(console.log).catch(console.log)
+
+// function updatePostById(update, postId) {
+//     const options = {
+//     method: 'PATCH',
+//     body: JSON.stringify(update),
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+// };
+
+// return fetch(`${BASE_URL}/posts/${postId}`, options).then(r => r.json())
+// }
+
+// updatePostById({ "title": "new test"},2)
+
+// function removePostBtId(postId) {
+//      const options = {
+//     method: 'DELETE',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+// };
+
+// return fetch(`${BASE_URL}/posts/${postId}`, options).then(r => r.json()) 
+// }
+
+// removePostBtId(2).then(console.log).catch(console.log)
