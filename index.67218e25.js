10ecc0a84e5c3bb41621b7553f634f96414928bd
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2022-08-16 17:53:11 +0000

    Deploying to gh-pages from @ serhiilukianenkodev/test-parsel@22254060bb20ff7a5532adb321af0d8515b09217 🚀

--- index.67218e25.js
+++
@@ -1,2 +0,0 @@
-!function(){var t,o;(t={title:"test 22",author:"Serhii dev"},o={method:"POST",body:JSON.stringify(t),headers:{"Content-Type":"application/json"}},fetch("".concat("http://localhost:3000","/posts/"),o).then((function(t){return t.json()}))).then(console.log).catch(console.log)}();
-//# sourceMappingURL=index.67218e25.js.map
