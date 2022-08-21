b261c6407182e23bb5d9ed5661c418f23b2d0f01
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2022-08-21 04:45:23 +0000

    Deploying to gh-pages from @ serhiilukianenkodev/test-parsel@1b2091f7f23378dab752a1b3442232fe2b1ce2d4 🚀

--- index.6b2931b6.js
+++
@@ -1,2 +0,0 @@
-console.log(function(o){const e={5:0,10:0};let c=!0;return o.forEach((o=>{switch(o){case 5:e[5]+=1;break;case 10:if(0===e[5])return c=!1;e[5]-=1,e[10]+=1;break;case 20:if(0!==e[10]&&0!==e[5]){e[10]-=1,e[5]-=1;break}if(e[5]<3)return c=!1;e[5]-=3}console.log(e)})),c}([5,5,5,5,20,20,5,5,20,5])),console.log("hola amigo");
-//# sourceMappingURL=index.6b2931b6.js.map
--- index.8371d816.js
+++
@@ -1,2 +0,0 @@
-!function(){var o,e;console.log((o={5:0,10:0},e=!0,[5,5,5,5,20,20,5,5,20,5].forEach((function(a){switch(a){case 5:o[5]+=1;break;case 10:if(0===o[5])return e=!1;o[5]-=1,o[10]+=1;break;case 20:if(0!==o[10]&&0!==o[5]){o[10]-=1,o[5]-=1;break}if(o[5]<3)return e=!1;o[5]-=3}console.log(o)})),e)),console.log("hola amigo")}();
-//# sourceMappingURL=index.8371d816.js.map
